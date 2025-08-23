4cdd6dc1e5dc603d1bd43ee4a090716c29360aa5
I'm definition of ordinary <iseungju711@gmail.com>
2025-08-23 18:03:28 +0900

    틱택토 기본틀

--- tictactoc.c
+++ tictactoc.c
@@ -1,32 +1,79 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(void){
 	
-	int number = 1;
+	int number[3][3] = {
+	{7,8,9},
+	{4,5,6},
+	{1,2,3}
+	};
+	
+	char imsi[4][4] = {0,};
 	
 	for(int i=0;i<3;i++){
 		
 		for(int j=0;j<3;j++){
-			printf("  %d",number);
-			
+			printf("  %d",number[i][j]);
+		
 			if((j+1) % 3 == 0){
 				printf("\n");
 			}else{
 				printf("  |");
 			}
-			
-			number++;
 		}
-		
 		if(i != 2)
 			printf("-----------------\n");
-		
 	}
+
+	printf("------------------------\n");
 	
 	int Onumber = 0;
 	int Xnumber = 0;
 	
-	scanf("%d",&Onumber);
+	while(1){
+		
+		scanf("%d",&Onumber);
+		
+		for(int c=0;c<3;c++){
+			for(int v=0;v<3;v++){
+				if(Onumber == number[c][v]){
+					imsi[c][v] = 'o';
+					printf("imsi[%d][%d] : %c\n",c,v,imsi[c][v]);
+				}
+			}
+		}
+		
+		scanf("%d",&Xnumber);
+		
+		for(int c=0;c<3;c++){
+			for(int v=0;v<3;v++){
+				if(Xnumber == number[c][v]){
+					imsi[c][v] = 'x';
+					printf("imsi[%d][%d] : %c\n",c,v,imsi[c][v]);
+				}
+			}
+		}
+		
+		for(int i=0;i<3;i++){
+		
+			for(int j=0;j<3;j++){
+				printf("  %c",imsi[i][j]);
+			
+				if((j+1) % 3 == 0){
+					printf("\n");
+				}else{
+					printf("  |");
+				}
+		}
+			if(i != 2)
+				printf("-----------------\n");
+		}
+	}
+	
+	
+	//fflush(stdout);
+	//system("cls");
 	
 	
 	return 0;
